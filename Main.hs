c5b9d229a6b0f7e10c3aa92d585f8b3022d6ef24
Francisco Nascimento <ffreiredonascimento1@gmail.com>
2022-02-03 18:09:35 +0000

    Tests option added

--- Main.hs
+++ Main.hs
@@ -3,29 +3,42 @@ import System.Environment
 import Caesar
 import Vigenere
 import Substitution
+import Tests
 import Control.Monad (unless, when)
 
 main = do
     -- Obtain and validate arguments
     args <- getArgs
-    -- arguments should be in the form:
-    -- method direction key
-    if head args `notElem` ["caesar","vigenere","substitution"]
+
+    if  length args `notElem` [1,3] 
         then 
-            putStrLn $ "Invalid method" ++ "\n" ++
-                       "Please use one of the following:" ++ "\n" ++
-                       " > caesar" ++ "\n" ++
-                       " > vigenere" ++ "\n" ++
-                       " > substitution"
-        else
-            if args!!1 `notElem` ["enc","dec"]
-                then 
-                    putStrLn $ "Invalid direction of encryption" ++ "\n" ++
-                               "Please use one of the following:" ++ "\n" ++
-                               " > enc" ++ "\n" ++
-                               " > dec"
-                else 
-                    applyAlgorithms args
+            putStrLn $ "Invalid arguments, try using:" ++ "\n" ++
+                       "./Main <method> <direction> <key>"
+                       ++ "or to run the tests use:\n" ++
+                       "./Main -t"
+        else 
+            -- Verify if user wants to run tests
+            if head args == "-t"
+                then test
+            else 
+                -- arguments should be in the form:
+                -- method direction key
+                if head args `notElem` ["caesar","vigenere","substitution"]
+                    then 
+                        putStrLn $ "Invalid method" ++ "\n" ++
+                                    "Please use one of the following:" ++ "\n" ++
+                                    " > caesar" ++ "\n" ++
+                                    " > vigenere" ++ "\n" ++
+                                    " > substitution"
+                    else
+                        if args!!1 `notElem` ["enc","dec"]
+                            then 
+                                putStrLn $ "Invalid direction of encryption" ++ "\n" ++
+                                            "Please use one of the following:" ++ "\n" ++
+                                            " > enc" ++ "\n" ++
+                                            " > dec"
+                            else 
+                                applyAlgorithms args
 
 applyAlgorithms args = do
                     let args1 = args!!1
